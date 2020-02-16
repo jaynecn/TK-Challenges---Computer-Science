f2f5c01a46ec960bc6e642857746d7ae5c0e6b69
Jayne Carmichael Norrie <39460834+jaynecn@users.noreply.github.com>
2020-02-16 19:42:25 +0000

    insertion sort and selection sort

---
+++ insertion_sort.py
@@ -0,0 +1,9 @@
+# What will the contents of the same array look like after each pass of the Insertion Sort algorithm?
+
+25 67 4 33 19 40
+
+25 67 4 33 19 40 # step 1 (no swaps)
+4 25 67 33 19 40 # step 2
+4 25 33 67 19 40 # step 3
+4 19 25 33 67 40 # step 4
+4 19 25 33 40 67 # step 5
---
+++ selection_sort.py
@@ -0,0 +1,9 @@
+# What will the contents of the array below look like after each pass of the Selection Sort algorithm?
+
+25 67 4 33 19 40
+
+4 67 25 33 19 40 # step 1
+4 19 25 33 67 40 # step 2
+4 19 25 33 67 40 # step 3 (no swaps)
+4 19 25 33 67 40 # step 4 (no swaps)
+4 19 25 33 40 67 # step 5
