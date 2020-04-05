22e48c31a6d16acb934775392b7445f8b519cae5
Jayne Carmichael Norrie <39460834+jaynecn@users.noreply.github.com>
2020-04-05 22:53:11 +0100

    dictionaries

--- python1.py
+++ python1.py
@@ -23,3 +23,49 @@ def check_fermat(a, b, c, n):
   else: print("No, that doesnt work")
   
 check_fermat(2,3,4,5)
+
+# Lists #
+colours = ['red', 'green', 'yellow']
+colours[0] = 'blue'
+print(colours)
+for i in range(0, len(colours)):
+  print(colours[i])
+print(colours.index('blue', 0))
+print(len(colours))
+colours.append('orange')
+print(colours)
+colours.insert(2, 'pink')
+print(colours)
+colours.remove('pink')
+print(colours)
+del colours[0]
+print(colours)
+
+vowel_string = 'aeiou'
+print(list(vowel_string))
+
+# Dictionaries #
+jayne = {"name": "Jayne", "age": 21, "year": 2020, "rich": "Nope"}
+jayne["rich"] = "yes"
+print(jayne)
+# print dictionary keys line by line #
+for key in jayne.keys():
+  print(key)
+
+# print dictionary values line by line #
+for value in jayne.values():
+  print(value)
+  
+print(jayne.values())
+
+# check index of an entry in dictionary #
+print("name" in jayne)
+print(len(jayne))
+
+# add item to dictionary #
+jayne["tired"] = "yes"
+print(jayne)
+jayne.pop("tired")
+print(jayne)
+jayne.clear()
+print(jayne)
