da7f63db8779d0990af31641531bf3463f960930
Jayne Carmichael Norrie <39460834+jaynecn@users.noreply.github.com>
2020-06-02 18:54:17 +0100

    improvements to parking lot TK exercise

--- python/parking_lot.py
+++ python/parking_lot.py
@@ -8,49 +8,79 @@ class User:
 class Warden(User):
   def __init__(self, name):
     super().__init__(name, is_admin=True)
-    valid_tickets = []
-  def check_tickets(self, ticket, customer):
-    checking = Tickets(self, name, is_valid)
-    if (is_valid == True):
-      print("Your ticket is valid.")
-      self.valid_tickets.append(checking)
-    else:
-      issue_ticket()
-  def issue_ticket():
+    self.valid_tickets = []
+  def __str__(self):
+    return(f"I'm a new admin.  My name is {self.name}")
+  def check_tickets(self, customer):
+    tickets = customer.purchases
+    print(f"{customer.name} tickets:")
+    print(tickets)
+    # checking = Tickets(ticket_name, ticket_price, ticket_is_valid)
+    # if (ticket_is_valid == True):
+    #   print("Your ticket is valid.")
+    #   self.valid_tickets.append(checking)
+    # else:
+    #   print("Your ticket isn't valid")
+  def issue_ticket(self):
     print("Your ticket isn't valid.  You have to pay a fine.")  
     
 class Customer(User):
   def __init__(self, name):
     super().__init__(name)
     self.purchases = []
-  def purchase_ticket(self, ticket):
-    purchase = Tickets(ticket, self)
+  def __str__(self):
+    return(f"Hello I'm a new customer.  My name is {self.name} .")
+  def purchase_ticket(self, ticket_name, ticket_price):
+    purchase = Tickets(self, ticket_name, ticket_price)
     self.purchases.append(purchase)
+    print(self.purchases)
+    
+jayne = Customer("Jayne")
+print(jayne)
+
+
+matt = Warden("matt")
+print(matt)
 
 class ParkingLot:
-  def__init__(self, name):
+  def __init__(self, name):
     self.name = name
     self.parking_lot = []
-  def car_in_parking_lot(self, ):
-    car_added = Car(car_registration, self)
+  def __str__(self):
+    return(f"New Parking lot created: {self.name}")
+  def car_in_parking_lot(self, registration):
+    car_added = Car(registration)
     self.parking_lot.append(car_added)
+    print(f"{registration} added to {self.name}")
+    print(self.parking_lot)
   def close_parking_lot(self):
-    if (len(parking_lot) == 100):
-      print("Parking lot is full")
-    else print ("Spaces")
+    if (len(self.parking_lot) == 100):
+      print(f"{self.name} parking lot is full")
+    else:
+      print(f"{self.name} parking lot: Spaces")
+      
+wood_street = ParkingLot("Wood Street")
+print(wood_street)
+wood_street.close_parking_lot()
 
 
 class Car:
-  def__init__(self, registration):
+  def __init__(self, registration):
     self.registration = registration
     
+wood_street.car_in_parking_lot('AB11')
+print(wood_street.parking_lot)
+    
 class Tickets:
-  def__init__(self, name, price, is_valid=True):
+  def __init__(self, name, price, is_valid=True):
     self.name = name
     self.price = price
     
+jayne.purchase_ticket('tuesday', 5)
+matt.check_tickets(jayne)
+    
 class Purchase:
-  def__init__(self, ticket, customer):
+  def __init__(self, ticket, customer):
     self.ticket = ticket
     self.customer = customer
     self.purchase_price = ticket.price
